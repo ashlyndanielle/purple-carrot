1907ca5d7836ccae7e9130f59d12d4fdf7629e8e
Ashlyn Mitros <ashlyndmitros@gmail.com>
2017-07-03 17:41:26 -0600

    added sass and bootstrap and react-router-dom

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { BrowserRouter } from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import './index.scss';
+
+import App from './components/App';
+
+
+ReactDOM.render(
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>,
+document.getElementById('root'));
 registerServiceWorker();
