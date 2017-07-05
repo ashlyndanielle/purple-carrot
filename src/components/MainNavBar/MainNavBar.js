d2446680b0474587b2ec376fea7383060d0bc97b
Ashlyn Mitros <ashlyndmitros@gmail.com>
2017-07-05 16:57:13 -0600

    added nav bar content for structure view

--- src/components/MainNavBar/MainNavBar.js
+++ src/components/MainNavBar/MainNavBar.js
@@ -1,12 +1,49 @@
 import React, { Component } from 'react';
 
-// import '../../../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import WhyPlants from '../WhyPlants/WhyPlants';
+import HowItWorks from '../HowItWorks/HowItWorks';
+import Menu from '../Menu/Menu';
+import Plans from '../Plans/Plans';
+import Tb12 from '../Tb12/Tb12';
+import Gifts from '../Gifts/Gifts';
+
+import '../../../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 class MainNavBar extends Component {
     render() {
         return (
             <div className='main-nav-container'>
-                Main Nav Bar
+                <nav className='mobile'>
+                    <div className='ham'>ham</div>
+                    <div className='logo'>logo</div>
+                </nav>
+                
+                <nav className='tablet'>
+                    <div className='left'>
+                        <div className='ham'>ham</div>
+                        <div className='logo'>logo</div>
+                    </div>
+                    <div className='right'>
+                        <p>login</p>
+                        <p>sign up</p>
+                    </div>
+                </nav>
+
+                <nav className='desktop'>
+                    <div className='menu-items'>
+                        <div>logo</div>
+                        <p>Why Plants</p>
+                        <p>How It Works</p>
+                        <p>Menu</p>
+                        <p>Plans</p>
+                        <p>Tb12</p>
+                        <p>Gifts</p>
+                    </div>
+                    <div className='login'>
+                        <p>login</p>
+                        <p>sign up</p>
+                    </div>
+                </nav>
             </div>
         );
     }
--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 import MainNavBar from './components/MainNavBar/MainNavBar';
+import Footer from './components/Footer/Footer';
 import Home from './components/Home/Home';
 import WhyPlants from './components/WhyPlants/WhyPlants';
 import HowItWorks from './components/HowItWorks/HowItWorks';
@@ -18,41 +19,48 @@ export default (
             <div>
                 <MainNavBar />
                 <Home/> 
+                <Footer />
             </div>
             ) } />
         <Route path='/why-plants' render={ () => (
             <div>
                 <MainNavBar />
                 <WhyPlants />
+                <Footer />
             </div>
         )} />
         <Route path='/how-it-works' render={ () =>(
             <div>
                 <MainNavBar />
                 <HowItWorks />
+                <Footer />
             </div>
         ) } /> 
         <Route path='/menu' render={ () => (
             <div>
                 <MainNavBar />
                 <Menu />
+                <Footer />
             </div>
         )} />
         <Route path='/plans' render={ () => (
             <div>
                 <MainNavBar />
                 <Plans />
+                <Footer />
             </div>
         )} />
         <Route path='/Tb12' render={ () => (
             <div>
                 <Tb12 />
+                <Footer />
             </div>
         )} />
         <Route path='/gifts' render={ () => (
             <div>
                 <MainNavBar />
                 <Gifts />
+                <Footer />
             </div>
         )} />
 
