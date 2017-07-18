83a35f1950bb9d6815a36b7bf293cc501297751b
Ashlyn Mitros <ashlyndmitros@gmail.com>
2017-07-18 12:34:29 -0600

    added content and some styling to footer component

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,11 +1,39 @@
 import React from 'react';
 
-import './Footer.scss';
+import facebook from '../../Images/footer/facebookIcon.png';
+import instagram from '../../Images/footer/instagramIcon.png';
+import pinterest from '../../Images/footer/pinterestIcon.png';
+import twitter from '../../Images/footer/twitterIcon.png';
+
+import './Footer.css';
 
 const Footer = () => {
     return (
         <div className='footer-main'>
-            Footer
+            <div className='social-media'>
+                <div className='social-links'>
+                    <a href="https://www.facebook.com/purplecarrotxo">
+                        <img src={facebook} alt=""/>
+                    </a>
+                </div>
+                <div className='social-links'>
+                    <a href="https://twitter.com/purplecarrotxo">
+                        <img src={twitter} alt=""/>
+                    </a>
+                </div>
+                <div className='social-links'>
+                    <a href="https://www.pinterest.com/thepurplecarrot/">
+                        <img src={pinterest} alt=""/>
+                    </a>
+                </div>
+                <div className='social-links'>
+                    <a href="https://www.instagram.com/purplecarrotxo/">
+                        <img src={instagram} alt=""/>
+                    </a>
+                </div>
+            </div>
+            <div className='links'></div>
+            <div className='copywrite'></div>
         </div>
     );
 };
