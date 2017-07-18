014c9b5c38b30bc271b2058e9f2d3b1fa9f4219f
Ashlyn Mitros <ashlyndmitros@gmail.com>
2017-07-18 10:52:00 -0600

    roughly finished world component in home view

--- src/components/Home/World.js
+++ src/components/Home/World.js
@@ -14,11 +14,27 @@ const World = () => {
             <div className='row'>
                 <div className='col-sm-6'>
                     <div className='ham-wrapper'>
-                        <img src={ham} alt=""/>
+                        <img className='ham-icon' src={ham} alt=""/>
                     </div>
                 </div>
-                <div className='col-sm-6'>
-                    
+                <div className='col-sm-6 text-side'>
+                    <h1>GOOD FOR <br/>THE WORLD</h1>
+                    <p className='text'>When you choose to eat plant-based meals, you're choosing to drastically reduce your environmental footprint.</p>
+                    <p className='text bold'>Eating a black bean burger vs. a regular burger:</p>
+                    <div className='row icon-bullets'>
+                        <div className='col-sm-4'>
+                            <img src={water} alt=""/>
+                            <p>CONSERVES WATER</p>
+                        </div>
+                        <div className='col-sm-4'>
+                            <img src={carbon} alt=""/>
+                            <p>REDUCES EMISSIONS</p>
+                        </div>
+                        <div className='col-sm-4'>
+                            <img src={farm} alt=""/>
+                            <p>PROTECTS FARMLAND</p>
+                        </div>
+                    </div>
                 </div>
             </div>
             <div className='col-sm-12 hidden-xs wedge'>
