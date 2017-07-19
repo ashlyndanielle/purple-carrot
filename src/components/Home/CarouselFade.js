bb88708749e4f50d3713c39065377423e6f9393f
Ashlyn Mitros <ashlyndmitros@gmail.com>
2017-07-19 11:06:38 -0600

    started testimonials component and carouselfade component

---
+++ src/components/Home/CarouselFade.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+
+import Slider from 'react-slick';
+
+import './Scss/CarouselFade.css';
+
+class CarouselFade extends Component {
+    render() {
+
+        let settings = {
+            infinite: true,
+            dots: true,
+            fade: true,
+            autoplay: true,
+            autoplaySpeed: 3000,
+            slidesToScroll: 1
+        }
+
+        return (
+            <div className='carousel-fade-main'>
+                <Slider {...settings} className='slider'>
+                    
+                </Slider>
+            </div>
+        );
+    }
+}
+
+export default CarouselFade;
---
+++ src/components/Home/Testimonials.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import CarouselFade from './CarouselFade';
+
+import './Scss/Testimonials.css';
+
+const Testimonials = () => {
+    return (
+        <div className='testimonials-main'>
+            <div className='carousel'></div>
+            <div className='purple-bolt'></div>
+            <div className='testimonials'></div>
+        </div>
+    );
+};
+
+export default Testimonials;
