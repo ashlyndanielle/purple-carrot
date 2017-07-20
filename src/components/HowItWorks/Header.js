2cc6330fa9958c2fd71eb2f3a8557601211c3228
Ashlyn Mitros <ashlyndmitros@gmail.com>
2017-07-20 11:56:04 -0600

    added desktop styling to plants header component

--- src/components/HowItWorks/Header.js
+++ src/components/HowItWorks/Header.js
@@ -6,29 +6,40 @@ import './Scss/Header.css';
 const Header = () => {
     return (
         <div className='works-header-main'>
-            <div className='mobile hidden-lg hidden-md'>
+            <div className='mobile hidden-lg hidden-md hidden-sm'>
                 <div>
                     <p>Nature creates.</p>
                     <p>We deliver.</p>
                     <p>You cook.</p>
                 </div>
             </div>
-            <div className='mobile-bottom hidden-lg hidden-md'>
+            <div className='mobile-bottom hidden-lg hidden-md hidden-sm'>
                 <div className='p-text'>
                     <p>SELECT YOUR PLAN</p>
                     <p>AND WE'LL DO THE REST.</p>
                 </div>
                 <div className='plans-button'>
-                    <Link to='plans' className='plans-link'>
+                    <Link to='/plans' className='plans-link'>
                         VIEW PLANS
                     </Link>
                 </div>
             </div>
-             {/* <div className='words-lg hidden-md hidden-sm'>
-                <p>Nature creates.</p>
-                <p>We deliver.</p>
-                <p>You cook.</p>
-            </div>  */}
+
+
+
+
+            <div className='desktop hidden-xs'>
+                <div className='title'>
+                    <p>Nature creates.</p>
+                    <p>We deliver.  You cook.</p>
+                </div>
+                <p className='sub-title'>SELELCT YOUR PLAN AND WE'LL DO THE REST</p>
+                <div className='large-button'>
+                    <Link to='/plans' className='plans-link'>
+                        VIEW PLANS
+                    </Link>
+                </div>
+            </div>  
         </div>
     );
 };
---
+++ src/components/HowItWorks/Ingredients.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './Scss/Ingredients.css';
+
+const Ingredients = () => {
+    return (
+        <div className='ingredients-main'>
+            
+        </div>
+    );
+};
+
+export default Ingredients;
